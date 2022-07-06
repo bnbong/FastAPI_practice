6033dbeda4629de314ae1648d806d726bb212756
bnbong <bbbong9@gmail.com>
2022-07-06 17:15:24 +0900

    serverside  testing  complete

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import uvicorn
+
 from typing import Union
 
 from fastapi import FastAPI
@@ -11,3 +13,9 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app",
+                host="serverside-testing.bnbongcodeserver.tk",
+                port=8000,
+                reload=True)
