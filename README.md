f511ff52a8bebb1c870f95705e87cabbedaecc09
bnbong <bbbong9@gmail.com>
2022-07-06 19:52:05 +0900

    practicing async function

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ practicing FastAPI
 
 ## venv deactivate
  > deactivate
+
+## running server
+ > http://serverside-testing.bnbongcodeserver.tk:8000/
+
+## API document
+ > http://serverside-testing.bnbongcodeserver.tk:8000/docs
+ > http://serverside-testing.bnbongcodeserver.tk:8000/redoc
+
+## fast API practice OpenAPI json
+ > http://serverside-testing.bnbongcodeserver.tk:8000/openapi.json
--- main.py
+++ main.py
@@ -14,6 +14,19 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+@app.get('/users/me')
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+@app.get("/users")
+async def read_users():
+    return ["Rick", "Morty"]
+    
+
 if __name__ == "__main__":
     uvicorn.run(app="main:app",
                 host="serverside-testing.bnbongcodeserver.tk",
