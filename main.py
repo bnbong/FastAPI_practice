a649652791c50eda518890d9bbb4946a91b1b0c2
bnbong <bbbong9@gmail.com>
2022-07-08 13:08:22 +0900

    practicing put method

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ from typing import Union
 from enum import Enum
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 
 app = FastAPI()
@@ -20,6 +26,10 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
 
 @app.get('/users/me')
 async def read_user_me():
